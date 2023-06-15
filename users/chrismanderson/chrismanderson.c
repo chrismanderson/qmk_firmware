4a39e2fc8c320faf0fc25c9e03acdbf68d70400c
Chris Anderson <chris.anderson@sweetgreen.com>
2023-06-15 16:59:19 -0400

    Add custom layout for Mode Sonnet

---
+++ users/chrismanderson/chrismanderson.c
@@ -0,0 +1,65 @@
+/* Copyright 2022 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "chrismanderson.h"
+
+bool is_siri_active = false;
+uint32_t siri_timer = 0;
+
+key_combination_t key_comb_list[4] = {
+    {2, {KC_LWIN, KC_TAB}},
+    {2, {KC_LWIN, KC_E}},
+    {3, {KC_LSFT, KC_LCMD, KC_4}},
+    {2, {KC_LWIN, KC_C}}
+};
+
+static uint8_t mac_keycode[4] = { KC_LOPT, KC_ROPT, KC_LCMD, KC_RCMD };
+
+void housekeeping_task_polutropon(void) {
+    if (is_siri_active) {
+        if (sync_timer_elapsed32(siri_timer) >= 500) {
+            unregister_code(KC_LCMD);
+            unregister_code(KC_SPACE);
+            is_siri_active = false;
+        }
+    }
+}
+
+bool process_record_polutropon(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_LOPTN:
+        case KC_ROPTN:
+        case KC_LCMMD:
+        case KC_RCMMD:
+            if (record->event.pressed) {
+                register_code(mac_keycode[keycode - KC_LOPTN]);
+            } else {
+                unregister_code(mac_keycode[keycode - KC_LOPTN]);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_SIRI:
+            HCS(0xCF);
+        case KC_SPOTLIGHT:
+            HCS(0x221);
+        case KC_MAC_DND:
+            HSS(0x9B);
+        case KC_MAC_LOCK:
+            HCS(0x19E);
+        default:
+            return true;  // Process all other keycodes normally
+    }
+}
---
+++ users/chrismanderson/chrismanderson.h
@@ -0,0 +1,49 @@
+/* Copyright 2022 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdint.h"
+#ifdef VIA_ENABLE
+#    include "via.h"
+#endif
+#include "quantum_keycodes.h"
+
+enum custom_keycodes {
+    KC_LOPTN = QK_KB_2, // TECH DEBT: Starts at QK_KB_2 to maintain ordering with VIA definitions. See #19884. Revert to QK_KB_0 when VIA catches up with QMK.
+    KC_ROPTN,
+    KC_LCMMD,
+    KC_RCMMD,
+    KC_SIRI,
+    KC_SPOTLIGHT,
+    KC_MAC_DND,
+    KC_MAC_LOCK,
+};
+
+#define KC_MCTL KC_MISSION_CONTROL
+#define KC_SPTL KC_SPOTLIGHT
+#define KC_LPAD KC_LAUNCHPAD
+#define KC_MDND KC_MAC_DND
+
+#define HSS(report) host_system_send(record->event.pressed ? report : 0); return false
+#define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
+
+
+typedef struct PACKED {
+    uint8_t len;
+    uint8_t keycode[3];
+} key_combination_t;
+
+void housekeeping_task_polutropon(void);
+bool process_record_polutropon(uint16_t keycode, keyrecord_t *record);
